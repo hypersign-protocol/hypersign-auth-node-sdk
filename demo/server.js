f2354589a02e7a12d49babad45a8b056b54370bb
vishwas1 <vishwasanand.cse@gmail.com>
2021-01-21 21:17:32 +0530

    implemented registration page in demo

--- demo/server.js
+++ demo/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 const HypersignAuth = require('hypersign-auth-js-sdk')
 
 const port = 4006
@@ -14,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static('public'))
 
 
 const options = {
@@ -26,13 +26,17 @@ const options = {
 }
 const hypersign = new HypersignAuth({
     server, // http server,
-    baseUrl: process.env.BASEURL || 'http://localhost:4006', // make sure you change this
+    baseUrl: process.env.BASEURL || 'http://192.168.43.43:4006', // make sure you change this
     options
 });
 
 // Unprotected resource, may be to show login page
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/register.html'));
 });
 
 // Implement /auth API: 
@@ -47,6 +51,34 @@ app.post('/hs/api/v2/auth', hypersign.authenticate.bind(hypersign), (req, res) =
         res.status(500).send({ status: 500, message: null, error: e.message });
     }
 })
+
+
+// Implement /register API: 
+// Analogous to register user but not yet activated
+app.post('/hs/api/v2/register', hypersign.register.bind(hypersign), (req, res) => {
+    try {
+        console.log('Register success');
+        // You can store userdata (req.body) but this user is not yet activated since he has not 
+        // validated his email.
+        res.status(200).send({ status: 200, message: "Success", error: null });
+    } catch (e) {
+        res.status(500).send({ status: 500, message: null, error: e.message });
+    }
+})
+
+
+// Implement /credential API: 
+// Analogous to activate user
+app.get('/hs/api/v2/credential', hypersign.issueCredential.bind(hypersign), (req, res) => {
+    try {
+        console.log('Credential success');
+        // Now you can make this user active
+        res.status(200).send({ status: 200, message: req.body.verifiableCredential, error: null });
+    } catch (e) {
+        res.status(500).send({ status: 500, message: null, error: e.message });
+    }
+})
+
 
 // Protected resource
 // Must pass hs_authorizationToken in x-auth-token header
