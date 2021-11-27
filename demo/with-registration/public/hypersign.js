887f188229ba8956e1bc16c52464b4b698754d0d
vishwas anand <vishwasanand.cse@gmail.com>
2021-11-27 22:54:31 +0530

    updated with frontend sdk

--- demo/with-registration/public/hypersign.js
+++ demo/with-registration/public/hypersign.js
@@ -22,14 +22,27 @@ async function register() {
         if (input.attr('type') == 'text') data["user"][input.attr('name')] = input.val();
     });
     console.log(data)
-    $.post(
-        `http://${window.location.host}/hs/api/v2/register`,
-        data,
-        (err, status) => {
-            $('span.loader').hide();
-            if (err) console.log(err)
-            alert('Your credential is sent to you in your email, scan the QR code to receive credential in your Hypersign Identity Wallet')
-        });
+    fetch(`http://${window.location.host}/hs/api/v2/register`,{
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((res) => {
+        console.log(res)
+        if(res.status != 200){
+            throw new Error("Invalid")
+        }
+        return res.json()
+    })
+    .then((json) =>{
+        alert('Your credential is sent to you in your email, scan the QR code to receive credential in your Hypersign Identity Wallet')
+    })
+    .catch(e => {
+        console.error(e)
+    })
+    
 }
 
 function formattedMessage(m) {
@@ -42,66 +55,36 @@ function formattedMessage(m) {
 function initiate() {
     $('.register').hide();
     $('.profile').hide();
-
-    let clientId = Math.floor(Math.random(10) * 100);
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-    if (!window.WebSocket) {
-        alert('Sorry, but your browser doesn\'t support WebSocket.')
-        return;
-    }
-
-    let ws = new WebSocket(`ws://${window.location.host}`);
-    ws.onopen = function() {};
-
-    ws.onmessage = function({
-        data
-    }) {
-        let messageData = JSON.parse(data);
-        console.log(messageData)
-        $("#qrcode").html("");
-        $('#profile').html('');
-        if (messageData.op == 'init') {
-            $("#qrcode").qrcode({
-                "width": 250,
-                "height": 250,
-                "text": JSON.stringify(messageData.data)
-            });
+    callAndGetUserProfile();
+}
 
 
-            const weblink = encodeURI(
-                "https://hswallet-stage.netlify.app/deeplink?url=" + JSON.stringify(messageData.data)
-            );
-            $(".web").html(
-                ` <button class='btn btn-primary' onclick="window.open('${weblink}', 'popUpWindow','height=800,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');">LOGIN USING WEB WALLET</button>`
-            );
-        } else if (messageData.op == 'end') {
-            ws.close();
-            $("#qrcode").hide();
-            const { accessToken, refreshToken } = messageData.data.hypersign.data
-            const url = `http://${window.location.host}/protected`
-            console.log(url)
-            fetch(url, {
-                    headers: {
-                        'Authorization': 'Bearer ' + accessToken
-                    },
-                    method: "POST"
-                }).then(res => res.json())
-                .then(json => {
-                    if (json.status == 403) {
-                        console.log('eroor 403')
-                    } else {
-                        $('.register').hide();
-                        $('.login').hide();
-                        $('.profile').show();
-                        $('#profile').html(JSON.stringify(json.message))
-                    }
-                })
-                .catch((e) => {
-                    console.log(e)
-                })
-        }
-    };
+function callAndGetUserProfile(){
 
-    ws.onerror = function(error) {};
+    document.addEventListener('hs-success', function (e) {
+        const { accessToken, refreshToken } = e.detail
+        const url = `http://${window.location.host}/protected`
+        console.log(url)
+        fetch(url, {
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                method: "POST"
+            }).then(res => res.json())
+            .then(json => {
+                if (json.status == 403) {
+                    console.log('eroor 403')
+                } else {
+                    $('.register').hide();
+                    $('.login').hide();
+                    $('.profile').show();
+                    $('#profile').html(JSON.stringify(json.message))
+                }
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    });
 
+            
 }
