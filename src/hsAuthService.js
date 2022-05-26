a92d2a8ae818df0766f99f751390ec41d52ff287
Vishwas <vishwasanand.cse@gmail.com>
2022-05-27 00:03:48 +0530

    updated package with latest ssi=sdk; integrated hid node

--- src/hsAuthService.js
+++ src/hsAuthService.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const hsSdk = require('hs-ssi-sdk');
+const HypersignSsiSDK = require('hs-ssi-sdk');
 const regMailTemplate = require('./mail/mail.template');
 const MailService = require('./mail/mail.service');
 const { clientStore, tokenStore, logger } = require('./config');
@@ -8,22 +8,32 @@ const { sanetizeUrl, getFormatedMessage, fetchData, responseMessageFormat } = re
 
 
 module.exports = class HypersignAuthService {
+
     constructor(options = {}, baseUrl) {
         this.options = {};
-        this.options.jwtExpiryTime = options ? options.jwt.expiryTime : 240000;
-        this.options.rftokenExpiryTime = options ? options.rft.expiryTime : 1000;
-        this.options.jwtSecret = options ? options.jwt.secret : 'secretKey';
-        this.options.rftokenSecret=options ? options.rft.secret :'8e5507e12da789f3c3bd640711378201d658657999384061bb';
-        this.options.hsNodeUrl = options ? options.networkUrl : 'https://ssi.hypermine.in/core'
-        this.options.mail = options ? options.mail : mail;
-        const hypersignSSISdk =  new hsSdk({nodeUrl: this.options.hsNodeUrl});
-        this.hsSdkVC = hypersignSSISdk.credential;
-        this.baseUrl = baseUrl;
+        this.options.jwtExpiryTime = options && options.jwt.expiryTime ? options.jwt.expiryTime : 240000;
+        this.options.rftokenExpiryTime = options && options.rft.expiryTime ? options.rft.expiryTime : 1000;
+        this.options.jwtSecret = options && options.jwt.secret ? options.jwt.secret : 'secretKey';
+        this.options.rftokenSecret = options && options.rft.secret ? options.rft.secret : '8e5507e12da789f3c3bd640711378201d658657999384061bb';
+        this.options.hidNodeURL = options && options.networkUrl ? options.networkUrl : "http://localhost:26657"
+        this.options.hidNodeRestURL = options && options.networkRestUrl ? options.networkRestUrl : "http://localhost:1317"
+        this.options.mail = options && options.mail ? options.mail : mail;
 
-        this.baseUrl =  sanetizeUrl(this.baseUrl);
-        this.options.hsNodeUrl =  sanetizeUrl(this.options.hsNodeUrl)
+        if (!options.offlineSigner) {
+            throw new Error('OfflineSigner is required for initilizing Hypersign Auth Service')
+        }
+        this.options.offlineSigner = options.offlineSigner
+
+        this.baseUrl = baseUrl;
+        this.baseUrl = sanetizeUrl(this.baseUrl);
+        this.options.hidNodeURL = sanetizeUrl(this.options.hidNodeURL)
+        this.options.hidNodeRestURL = sanetizeUrl(this.options.hidNodeRestURL)
 
         this.options.keys = options.keys;
+
+        // const hsSdk = new hsSdk({ nodeUrl: this.options.hidNodeURL });
+
+
         this.options.schemaId = options.schemaId;
 
         this.options.mail = options.mail;
@@ -39,10 +49,19 @@ module.exports = class HypersignAuthService {
         this.isSubscriptionSuccess = false;
         this.isSubcriptionEnabled = options.isSubcriptionEnabled;
 
-        this.verifyResourcePath = this.options.appCredential.credentialSubject.verifyResourcePath != "" ? 
-                                ( this.options.appCredential.credentialSubject.verifyResourcePath.startsWith("/") ? this.options.appCredential.credentialSubject.verifyResourcePath : "/" + this.options.appCredential.verifyResourcePath) 
-                                : "/hs/api/v2/credential";
+        this.verifyResourcePath = this.options.appCredential.credentialSubject.verifyResourcePath != "" ?
+            (this.options.appCredential.credentialSubject.verifyResourcePath.startsWith("/") ? this.options.appCredential.credentialSubject.verifyResourcePath : "/" + this.options.appCredential.verifyResourcePath) :
+            "/hs/api/v2/credential";
     }
+
+
+    async init() {
+        const hsSdk = new HypersignSsiSDK(this.options.offlineSigner, this.options.hidNodeURL, this.options.hidNodeRestURL);
+        await hsSdk.init();
+        this.hsSdkVC = hsSdk.vc;
+        this.hsSDKVP = hsSdk.vp;
+    }
+
     /**
      * Verifies VP
      * @param { Object } vpObj  // verifiable presentation
@@ -53,13 +72,15 @@ module.exports = class HypersignAuthService {
         if (!vpObj) throw new Error('presentation is null')
         if (!challenge) throw new Error('challenge is null')
         const vc = vpObj.verifiableCredential[0];
-        const isVerified = await this.hsSdkVC.verifyPresentation({
-            presentation: vpObj,
-            challenge: challenge,
-            issuerDid: vc.proof.verificationMethod,
-            holderDid: vpObj.proof.verificationMethod
-        });
-        return isVerified.verified;
+        const result = await this.hsSDKVP.verifyPresentation({
+            signedPresentation: vpObj,
+            challenge,
+            domain: "https://localhos:20202", //TODO:  need to remove this hardcoding
+            issuerDid: vc.issuer,
+            holderDid: vc.credentialSubject.id,
+        })
+        const { verified } = result;
+        return verified;
     }
 
     /**
@@ -68,7 +89,8 @@ module.exports = class HypersignAuthService {
      * @returns signed VC
      */
     async generateCredential(userData) {
-        const schemaUrl = this.options.hsNodeUrl + '/api/v1/schema/' + this.options.schemaId;
+        // const schemaUrl = this.options.hidNodeURL + '/api/v1/schema/' + this.options.schemaId;
+        const schemaId = this.options.schemaId;
         const issuerKeys = this.options.keys;
         const { did } = userData;
 
@@ -78,15 +100,22 @@ module.exports = class HypersignAuthService {
         delete userData['did'];
 
         logger.debug("HS-AUTH:: Credential is being generated...")
-        const credential = await this.hsSdkVC.generateCredential(schemaUrl, {
+
+        const options = {
+            schemaId,
             subjectDid: did,
             issuerDid: issuerKeys.publicKey.id,
             expirationDate: new Date().toISOString(),
-            attributesMap: userData,
-        })
-
+            fields: userData
+        }
+        const credential = await this.hsSdkVC.getCredential(options)
         logger.debug("HS-AUTH:: Credential is being signed...")
-        const signedCredential = await this.hsSdkVC.signCredential(credential, issuerKeys.publicKey.id, issuerKeys.privateKeyBase58)
+        const signOptions = {
+            credential,
+            issuerDid: issuerKeys.publicKey.id,
+            privateKey: issuerKeys.privateKeyBase58
+        }
+        const signedCredential = await this.hsSdkVC.signCredential(signOptions)
         return signedCredential
     }
 
@@ -172,11 +201,11 @@ module.exports = class HypersignAuthService {
      */
     async authenticate(body) {
         const { challenge, vp } = body;
-        if(this.isSubcriptionEnabled){
+        if (this.isSubcriptionEnabled) {
             await this.checkSubscription();
             if (!this.isSubscriptionSuccess) throw new Error('Subscription check unsuccessfull')
         }
-        
+
         const vpObj = JSON.parse(vp);
         const subject = vpObj['verifiableCredential'][0]['credentialSubject'];
 
@@ -187,7 +216,7 @@ module.exports = class HypersignAuthService {
         // TODO:  need to find out if we are missing any imp parameter in the options.
         // what is the proper way to JWT sign 
         const accessToken = await jwt.sign(subject, this.options.jwtSecret, { expiresIn: this.options.jwtExpiryTime });
-        const refreshToken= await jwt.sign(subject, this.options.rftokenSecret, { expiresIn: this.options.rftokenExpiryTime })
+        const refreshToken = await jwt.sign(subject, this.options.rftokenSecret, { expiresIn: this.options.rftokenExpiryTime })
 
         // TODO:  once we use redis , we can set the expiration time = this.options.rftokenExpiryTime also
         // but for in-mem, let;s keep it simple
@@ -195,8 +224,8 @@ module.exports = class HypersignAuthService {
 
         let client = clientStore.getClient(challenge)
         const tokens = { accessToken, refreshToken }
-        if(client.connection){
-            client.connection.sendUTF(getFormatedMessage('end', responseMessageFormat(true, 'User is authenticated', tokens )))
+        if (client.connection) {
+            client.connection.sendUTF(getFormatedMessage('end', responseMessageFormat(true, 'User is authenticated', tokens)))
             client = clientStore.updateClient(challenge, client.connection, true, tokens.accessToken, tokens.refreshToken);
             clientStore.deleteClient(client.clientId);
         } else {
@@ -214,13 +243,13 @@ module.exports = class HypersignAuthService {
      * @param { string } refreshToken 
      * @returns accessToken and refreshToken
      */
-    async refresh(refreshToken){
+    async refresh(refreshToken) {
         const payload = await this.verifyRefreshToken(refreshToken)
 
         // TODO: we need to check if this refresh token was present in the store.
         const refTokenStored = await tokenStore.get(payload.id)
-        
-        if(refTokenStored != refreshToken){
+
+        if (refTokenStored != refreshToken) {
             throw new Error("Unauthorized: Invalid ref token or expired")
         }
 
@@ -234,7 +263,7 @@ module.exports = class HypersignAuthService {
         // TODO:  once we use redis , we can set the expiration time = this.options.rftokenExpiryTime also
         // but for in-mem, let;s keep it simple
         await tokenStore.set(payload.id, refToken, this.options.rftokenExpiryTime) // the expirey time is in second
-        
+
         return {
             accessToken,
             refreshToken: refToken
@@ -245,10 +274,10 @@ module.exports = class HypersignAuthService {
      * Deletes refresh token for that user
      * @param { string } refreshToken 
      */
-    async logout(refreshToken){
+    async logout(refreshToken) {
         const payload = await this.verifyRefreshToken(refreshToken)
-        // TODO: delete on logout
-        await tokenStore.delete(payload.id) 
+            // TODO: delete on logout
+        await tokenStore.delete(payload.id)
     }
 
     /**
@@ -259,7 +288,7 @@ module.exports = class HypersignAuthService {
     async authorize(authToken) {
         return await jwt.verify(authToken, this.options.jwtSecret)
     }
-    
+
     /**
      * Geneartes verifiable credential JWT and sends email
      * @param { object } user 
@@ -267,16 +296,16 @@ module.exports = class HypersignAuthService {
      * @returns null
      */
     async register(user, isThridPartyAuth = false) {
-        if(!this.mailService) throw new Error("Mail configuration is not defined");
-        if(!this.verifyResourcePath) throw new Error("VerifyResourcePath is not set in configuration file")
+        if (!this.mailService) throw new Error("Mail configuration is not defined");
+        if (!this.verifyResourcePath) throw new Error("VerifyResourcePath is not set in configuration file")
 
-        if(!user)  throw new Error("User object is null or empty.")
+        if (!user) throw new Error("User object is null or empty.")
 
-        if(isThridPartyAuth){
+        if (isThridPartyAuth) {
             const { did } = user;
-            
-            if(!did) throw new Error("Did must be passed with thirdparty auth request");
-            
+
+            if (!did) throw new Error("Did must be passed with thirdparty auth request");
+
             const verifiableCredential = await this.generateCredential(user);
             return verifiableCredential;
         }
@@ -293,12 +322,12 @@ module.exports = class HypersignAuthService {
         });
 
         // TODO:  need to remove this hardcoded url
-        const authServerOrigin = (new URL(this.options.hsNodeUrl)).origin;
+        const authServerOrigin = (new URL(this.options.hidNodeURL)).origin;
         const authenticationServerEndPoint = `${authServerOrigin}/hsauth`
         const deepLinkUrl = encodeURI(`${authenticationServerEndPoint}/deeplink.html?deeplink=hypersign:deeplink?url=${JSONdata}`);
         mailTemplate = mailTemplate.replace("@@DEEPLINKURL@@", deepLinkUrl);
-        
-        if(!user.email) throw new Error("No email is passed. Email is required property");
+
+        if (!user.email) throw new Error("No email is passed. Email is required property");
         const info = await this.mailService.sendEmail(user.email, mailTemplate, `${this.options.mail.name} Auth Credential Issuance`);
         return null;
     }
@@ -316,20 +345,20 @@ module.exports = class HypersignAuthService {
         return verifiableCredential
     }
 
-    async poll({ challenge }){
-        if(!challenge){
+    async poll({ challenge }) {
+        if (!challenge) {
             throw new Error("Challenge must be passed");
         }
         let client = clientStore.getClient(challenge);
-        if(!client){
+        if (!client) {
             throw new Error("Invalid challenge");
         }
         const { isAuthenticated, accessToken, refreshToken } = client;
-        if(isAuthenticated === false){
+        if (isAuthenticated === false) {
             throw new Error("Unauthorized");
         }
 
         clientStore.deleteClient(challenge);
-        return  { accessToken, refreshToken };
+        return { accessToken, refreshToken };
     }
 }
