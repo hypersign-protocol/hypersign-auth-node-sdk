3ba4d0b935c84c332b846cb025304322c9a4dd34
Vishwas <vishwasanand.cse@gmail.com>
2022-05-27 17:09:10 +0530

    integrating hid-node; work left.

--- demo/server.js
+++ demo/server.js
@@ -1,92 +1,38 @@
 const http = require('http')
 const express = require('express')
-const cors = require('cors');
+const { createHIDWallet } = require('hypersign-wallet-sdk');
 const HypersignAuth = require('hypersign-auth-node-sdk')
-
+const authRoutes = require('./routes')
+const mnemonic = "retreat seek south invite fall eager engage endorse inquiry sample salad evidence express actor hidden fence anchor crowd two now convince convince park bag"
 const port = 4006
 const app = express()
 const server = http.createServer(app)
 
 const TIME = () => new Date();
 app.use(express.json());
-app.use(cors());
 app.use(express.static("public"));
 
-const hypersign = new HypersignAuth(server);
 
-// Render Login page
-app.get("/", (req, res) => {
-    res.sendFile("index.html");
-});
+let hypersign;
+createHIDWallet(mnemonic).then(async(offlineSigner) => {
+        const accounts = await offlineSigner.getAccounts();
+        console.log('Hid Wallet Initialized hidAddess  = ' + accounts[0].address)
+        hypersign = new HypersignAuth(server, offlineSigner)
+        console.log(hypersign.authenticate)
+        await hypersign.init();
+        console.log('Hypersign Auth service has been initialized')
 
-// Implement authentication API
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignauthenticate
-app.post('/hs/api/v2/auth', hypersign.authenticate.bind(hypersign), (req, res) => {
-    try {
-        const { user } = req.body.hypersign.data;
-        console.log(user)
-            // Do something with the user data.
-            // The hsUserData contains userdata and authorizationToken
-        res.status(200).send({ status: 200, message: "Success", error: null });
-    } catch (e) {
-        res.status(500).send({ status: 500, message: null, error: e.message });
-    }
-})
+        // Render Login page
+        app.get("/", (req, res) => {
+            res.sendFile("index.html");
+        });
 
-// Implement /register API: 
-// Analogous to register user but not yet activated
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignregister
-app.post('/hs/api/v2/register', hypersign.register.bind(hypersign), (req, res) => {
-    try {
-        console.log('Register success');
-        // You can store userdata (req.body) but this user is not yet activated since he has not 
-        // validated his email.
-        res.status(200).send({ status: 200, message: "Success", error: null });
-    } catch (e) {
-        res.status(500).send({ status: 500, message: null, error: e.message });
-    }
-})
+        app.use(authRoutes(hypersign))
 
-// Implement /credential API: 
-// Analogous to activate user
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignissuecredential
-app.get('/hs/api/v2/credential', hypersign.issueCredential.bind(hypersign), (req, res) => {
-    try {
-        console.log('Credential success');
-        const { hypersign } = req.body;
-        const { data } = hypersign;
-        console.log(hypersign)
-        res.status(200).send({ ...data });
-    } catch (e) {
-        res.status(500).send({ status: 500, message: null, error: e.message });
-    }
-})
-
-// Any resource which you want to protect
-// Must pass Authorization: Bearer <accessToken>  as header
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignauthorize
-app.post('/protected', hypersign.authorize.bind(hypersign), (req, res) => {
-    try {
-        const user = req.body.hypersign.data;
-        console.log(user)
-            // Do whatever you want to do with it
-        res.status(200).send({ status: 200, message: user, error: null });
-    } catch (e) {
-        res.status(500).send(e.message)
-    }
-})
-
-// New session
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignchallenge
-app.post("/challenge", hypersign.challenge.bind(hypersign), (req, res) => {
-    res.status(200).send(req.body);
-});
-  
-// Polling if authentication finished
-// Doc: https://github.com/hypersign-protocol/hypersign-auth-js-sdk/blob/master/docs.md#hypersignpoll
-app.get("/poll", hypersign.poll.bind(hypersign), (req, res) => {
-    res.status(200).send(req.body);
-});
+    })
+    .catch(e => {
+        console.error(e)
+    })
 
 server.listen(port, () => {
     console.log(`${TIME()} The server is running on port : ${port}`)
