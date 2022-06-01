5d34260e17d951da5bf430aa68ef81f05a4954ab
Vishwas <vishwasanand.cse@gmail.com>
2022-06-01 10:09:20 +0530

    checking dist for npm  git

--- demo/server.js
+++ demo/server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const express = require('express')
-const { createHIDWallet } = require('hypersign-wallet-sdk');
+const HIDWallet = require('hid-hd-wallet');
 const HypersignAuth = require('hypersign-auth-node-sdk')
 const authRoutes = require('./routes')
 const mnemonic = "retreat seek south invite fall eager engage endorse inquiry sample salad evidence express actor hidden fence anchor crowd two now convince convince park bag"
@@ -28,10 +28,9 @@ app.use(express.static("public"));
 
 
 let hypersign;
-createHIDWallet(mnemonic).then(async(offlineSigner) => {
-        const accounts = await offlineSigner.getAccounts();
-        console.log('Hid Wallet Initialized hidAddess  = ' + accounts[0].address)
-        hypersign = new HypersignAuth(server, offlineSigner)
+const hidWalletInstance = new HIDWallet(walletOptions);
+hidWalletInstance.generateWallet({ mnemonic }).then(async() => {
+        hypersign = new HypersignAuth(server, hidWalletInstance.offlineSigner)
         console.log(hypersign.authenticate)
         await hypersign.init();
         console.log('Hypersign Auth service has been initialized')
